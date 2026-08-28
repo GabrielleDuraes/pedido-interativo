224a20f267b93507743eb945796c7dbbeec17cab
GABRIELLE DURAES <gabrielleduraes@hotmail.com>
2026-08-28 15:04:59 -0300

    Fix button ID for 'Não' to 'NÃO' in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let etapa = 1;
 const botaoSim = document.getElementById ("SIMMM");
-const botaoNao = document.getElementById("Não");
+const botaoNao = document.getElementById("NÃO");
 const pergunta = document.getElementById("pergunta");
 botaoSim.addEventListener("click", function() {
   if (etapa===1) {
